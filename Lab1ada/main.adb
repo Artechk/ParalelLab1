ce02f7a5d45d66f1e68a9e219b905a2484442442
Artechk <36381551+Artechk@users.noreply.github.com>
2024-04-30 00:50:08 +0300

    ada fixed

--- Lab1ada/main.adb
+++ Lab1ada/main.adb
@@ -1,116 +1,39 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Numerics.Discrete_Random;
-
+with Ada.Text_IO;
 
 procedure Main is
+   Can_Stop : Boolean := False; -- Variable to control stopping condition
+   pragma Atomic (Can_Stop); -- Making assignment atomic for safe concurrent access
+   Step : Long_Long_Integer := 1; -- Step value for each addition
 
-   Size : constant Integer := 1000;
-   Thread_Num : constant Integer := 6;
+   task type Stop_Thread;
+   task type Sum_Thread(ID:Integer);
 
-   Arr : array(1..Size) of Integer;
-   Min_Element : Integer := Integer'Last;
-   Min_Index : Integer := 1;
-
-
-
-
-   procedure Init_Arr is
-      subtype Random_Range is Integer range 1 .. 10000;
-      package R is new Ada.Numerics.Discrete_Random (Random_Range);
-      use R;
-
-      G : Generator;
-      X : Random_Range;
+   task body Stop_Thread is
    begin
-      Reset(G);
+      delay 3.0; -- Delay to stop the process after 3 seconds
+      Can_Stop := True; -- Set the stopping flag to True
+   end Stop_Thread;
 
-      for I in 1..Size loop
-         X := Random(G);
-         Arr(I) := X;
-
-            if X mod 2 = 0 then
-             Arr(I) := -Arr(I);
-            end if;
+   task body Sum_Thread is
+      Additions : Long_Long_Integer := 0; -- Counter for the number of additions
+      Sum : Long_Long_Integer := 0; -- Accumulator for the sum
+   begin
+      loop
+         exit when Can_Stop; -- Exit loop if Can_Stop is True
+         Sum := Sum + (Additions * Step); -- Calculate the sum
+         Additions := Additions + 1; -- Increment the counter
       end loop;
-end Init_Arr;
 
+      -- Print the result of the thread
+      Ada.Text_IO.Put_Line("Thread " & ID'Img & " - Sum: " & Sum'Img & " - Additions: " & Additions'Img);
+   end Sum_Thread;
 
-   procedure Part_Min(Start_Index, Finish_Index : in Integer) is
-   begin
-      for I in Start_Index..Finish_Index loop
-         if Arr(I) < Min_Element then
-            Min_Element := Arr(I);
-            Min_Index := I;
-         end if;
-      end loop;
-   end Part_Min;
+   B1 : Stop_Thread; -- Instantiate the stopping thread
 
-   task type Starter_Thread is
-      entry Start(Start_Index, Finish_Index : in Integer);
-   end Starter_Thread;
-
-   protected Part_Manager is
-      procedure Set_Part_Min(Element : in Integer; Index : in Integer);
-      entry Get_Min(Element : out Integer; Index : out Integer);
-   private
-      Tasks_Count : Integer := 0;
-      Min_Element1 : Integer := Integer'Last;
-      Min_Index1 : Integer := 1;
-   end Part_Manager;
-
-   protected body Part_Manager is
-      procedure Set_Part_Min(Element : in Integer; Index : in Integer) is
-      begin
-         if Element < Min_Element1 then
-            Min_Element1 := Element;
-            Min_Index1 := Index;
-         end if;
-         Tasks_Count := Tasks_Count + 1;
-      end Set_Part_Min;
-
-      entry Get_Min(Element : out Integer; Index : out Integer) when Tasks_Count = Thread_Num is
-      begin
-         Element := Min_Element1;
-         Index := Min_Index1;
-      end Get_Min;
-
-   end Part_Manager;
-
-   task body Starter_Thread is
-      Start_Index, Finish_Index : Integer;
-   begin
-      accept Start(Start_Index, Finish_Index : in Integer) do
-         Starter_Thread.Start_Index := Start_Index;
-         Starter_Thread.Finish_Index := Finish_Index;
-      end Start;
-      Part_Min(Start_Index  => Start_Index,
-               Finish_Index => Finish_Index);
-      Part_Manager.Set_Part_Min(Min_Element, Min_Index);
-   end Starter_Thread;
-
-   function Parallel_Min return Integer is
-      Min : Integer := Integer'Last;
-      Index : Integer := 1;
-      Thread : array(1..Thread_Num) of Starter_Thread;
-   begin
-      for I in 1..Thread_Num loop
-         Thread(I).Start((I - 1) * Size / Thread_Num + 1, I * Size / Thread_Num);
-      end loop;
-      Part_Manager.Get_Min(Min, Index);
-      return Min;
-   end Parallel_Min;
-
-
-
-
-
+   Threads : array(1..6) of access Sum_Thread; -- Array to hold references to Sum_Thread tasks
 begin
-
-   Init_Arr;
-   Put_Line("The number of elements: "&Size'Img);
-   Put_line("The number of threads: "&Thread_Num'Img);
-   Put_Line("Minimum element: " & Parallel_Min'Img);
-   Put_Line("Index of minimum element: " & Min_Index'Img);
-
-
+   -- Create six Sum_Thread tasks
+   for I in 1..6 loop
+      Threads(I) := new Sum_Thread(I);
+   end loop;
 end Main;
